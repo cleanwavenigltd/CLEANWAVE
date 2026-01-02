e2122e71c8348bdf739651d21d6ceed698245330
mmdmuazu <muhammadaliyumuazu@gmail.com>
2026-01-02 20:08:36 +0100

    initial commit

---
+++ COMPANY/WEB/web-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cleanwavenigltd.app',
+  appName: 'Cleanwave Recycling',
+  webDir: 'dist'
+};
+
+export default config;
